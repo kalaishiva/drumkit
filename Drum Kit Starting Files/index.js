1ccbb8df4b1318a2c2cb2aa1ad87bfbc4f8ae555
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-06-20 22:19:03 +0200

    drumkit completed

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -3,10 +3,47 @@ let numOfDrumButton = document.querySelectorAll('.drum').length; //selected all 
 for(i=0; i<numOfDrumButton; i++){
 
   // console.log(document.querySelectorAll('.drum')[i]); //selected all the button using iteration
-document.querySelectorAll('.drum')[i].addEventListener('click', handleClick);
+document.querySelectorAll('.drum')[i].addEventListener('click', function(){;
+/* this.style.color="white"; */
+let buttonInnerHTML = this.innerHTML;
+switch(buttonInnerHTML) {
+    case 'w': 
+        let tom1 =  new Audio('sounds/tom-1.mp3');
+        tom1.play();
+    break; 
 
-function handleClick(){
-    alert('The button is clicked');
+    case 'a': 
+        let tom2 =  new Audio('sounds/tom-2.mp3');
+        tom2.play();
+    break;
+
+    case 's': 
+        let tom3 =  new Audio('sounds/tom-3.mp3');
+        tom3.play();
+    break;
+    case 'd': 
+        let crash =  new Audio('sounds/crash.mp3');
+        crash.play();
+    break;
+    case 'j': 
+        let kick =  new Audio('sounds/kick-bass.mp3');
+        kick.play();
+    break;
+    case 'k': 
+        let snare =  new Audio('sounds/snare.mp3');
+        snare.play();
+    break;
+    case 'l': 
+        let tom4 =  new Audio('sounds/tom-4.mp3');
+        tom4.play();
+    break;
+
+    default : console.log(buttonInnerHTML);
+
 }
-} 
 
+/* function handleClick(){
+    
+} */
+});
+}
